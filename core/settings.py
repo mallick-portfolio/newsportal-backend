332586f20be0dbef78edf33a4930c17cd7cfc676
mallick-portfolio <tamal1714019@gmail.com>
2024-01-20 00:14:15 +0600

    update cors

--- core/settings.py
+++ core/settings.py
@@ -46,12 +46,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5000",
     "http://127.0.0.1:5000",
 ]
-# CSRF_TRUSTED_ORIGINS  = [
-#     'https://newsportal-hdb5.onrender.com',
-#     "http://127.0.0.1:3000",
-#     "http://127.0.0.1:5000",
-# ]
-# CORS_ALLOW_ALL_ORIGINS=["*"]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 
 
