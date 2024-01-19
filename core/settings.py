2702f2936bd4fc65115b0d0c61c5e9fd82152388
mallick-portfolio <tamal1714019@gmail.com>
2024-01-19 22:23:00 +0600

    update setting.py

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
+
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5000",
 ]
