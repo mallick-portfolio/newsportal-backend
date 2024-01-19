c093e1257a452dacd1c68b7ddda56dd1feb5ca80
mallick-portfolio <tamal1714019@gmail.com>
2024-01-19 22:25:04 +0600

    added allow host

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*', 'newsportal-hdb5.onrender.com']
 CORS_ALLOWED_ORIGINS = [
 
     "http://127.0.0.1:3000",
